cdc78a0fe4cacacbe13f78261ae53e1e4d7ae7e2
qianyeyixia <1521341740@qq.com>
2021-12-15 00:10:26 +0800

    部分提交

--- pages/canvas/cropper.js
+++ pages/canvas/cropper.js
@@ -1,66 +1,212 @@
 // pages/canvas/cropper.js
+
+const app = getApp()
 Page({
 
 	/**
 	 * 页面的初始数据
 	 */
 	data: {
-
+		imgSrc: null,
+		width: 250, //宽度
+		height: 250, //高度
+		max_width: 300,
+		max_height: 300,
+		disable_rotate: false, //是否禁用旋转
+		disable_ratio: false, //锁定比例
+		limit_move: false, //是否限制移动
 	},
-
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
 	onLoad: function (options) {
-
+		this.cropper = this.selectComponent("#image-cropper");
+		this.setData({
+			imgSrc: app.globalData.imgaeSrc
+		})
+		if (!options.imgSrc) {
+			this.cropper.upload(); //上传图片
+		}
 	},
-
-	/**
-	 * 生命周期函数--监听页面初次渲染完成
-	 */
-	onReady: function () {
-
+	clickcut(e) {
+		console.log(e.detail);
+		wx.previewImage({
+			current: e.detail.url, // 当前显示图片的http链接
+			urls: [e.detail.url], // 需要预览的图片http链接列表
+		});
 	},
-
-	/**
-	 * 生命周期函数--监听页面显示
-	 */
-	onShow: function () {
-
+	cropperload(e) {
+		console.log("cropper初始化完成");
 	},
-
-	/**
-	 * 生命周期函数--监听页面隐藏
-	 */
-	onHide: function () {
-
+	loadimage(e) {
+		wx.hideLoading();
+		console.log('图片');
+		this.cropper.imgReset();
 	},
-
-	/**
-	 * 生命周期函数--监听页面卸载
-	 */
-	onUnload: function () {
-
+	upload() {
+		let that = this;
+		wx.chooseImage({
+			count: 1,
+			sizeType: ['original', 'compressed'],
+			sourceType: ['album', 'camera'],
+			success(res) {
+				wx.showLoading({
+					title: '加载中',
+				})
+				const uploadTask = wx.uploadFile({
+					filePath: res.tempFilePaths[0],
+					name: "file",
+					url: `${app.globalData.baseUrl}/wx/photo/getForeground`,
+					formData: {
+						openId: app.globalData.userInfo.openId,
+					},
+					success: (fileRes) => {
+						console.log("success", fileRes);
+						let _data = JSON.parse(fileRes.data);
+						that.setData({
+							imgSrc: `data:image/png;base64,${_data.result}`,
+						});
+						app.globalData.imgaeSrc = that.data.imgSrc
+					},
+					fail: (res) => {
+						console.log("fail res", res);
+					},
+				});
+				uploadTask.onProgressUpdate((res) => {
+					console.log("上传进度", res.progress);
+					console.log("已经上传的数据长度", res.totalBytesSent);
+					console.log("预期需要上传的数据总长度", res.totalBytesExpectedToSend);
+				});
+				//重置图片角度、缩放、位置
+				that.cropper.imgReset();
+			}
+		})
 	},
-
-	/**
-	 * 页面相关事件处理函数--监听用户下拉动作
-	 */
-	onPullDownRefresh: function () {
-
+	setWidth(e) {
+		this.setData({
+			width: e.detail.value < 10 ? 10 : e.detail.value
+		});
+		this.setData({
+			cut_left: this.cropper.data.cut_left
+		});
 	},
-
-	/**
-	 * 页面上拉触底事件的处理函数
-	 */
-	onReachBottom: function () {
-
+	setHeight(e) {
+		this.setData({
+			height: e.detail.value < 10 ? 10 : e.detail.value
+		});
+		this.setData({
+			cut_top: this.cropper.data.cut_top
+		});
 	},
-
-	/**
-	 * 用户点击右上角分享
-	 */
-	onShareAppMessage: function () {
-
-	}
+	switchChangeDisableRatio(e) {
+		//设置宽度之后使剪裁框居中
+		this.setData({
+			disable_ratio: e.detail.value
+		});
+	},
+	setCutTop(e) {
+		this.setData({
+			cut_top: e.detail.value
+		});
+		this.setData({
+			cut_top: this.cropper.data.cut_top
+		});
+	},
+	setCutLeft(e) {
+		this.setData({
+			cut_left: e.detail.value
+		});
+		this.setData({
+			cut_left: this.cropper.data.cut_left
+		});
+	},
+	switchChangeDisableRotate(e) {
+		//开启旋转的同时不限制移动
+		if (!e.detail.value) {
+			this.setData({
+				limit_move: false,
+				disable_rotate: e.detail.value
+			});
+		} else {
+			this.setData({
+				disable_rotate: e.detail.value
+			});
+		}
+	},
+	switchChangeLimitMove(e) {
+		//限制移动的同时锁定旋转
+		if (e.detail.value) {
+			this.setData({
+				disable_rotate: true
+			});
+		}
+		this.cropper.setLimitMove(e.detail.value);
+	},
+	switchChangeDisableWidth(e) {
+		this.setData({
+			disable_width: e.detail.value
+		});
+	},
+	switchChangeDisableHeight(e) {
+		this.setData({
+			disable_height: e.detail.value
+		});
+	},
+	submit() {
+		this.cropper.getImg((obj) => {
+			app.globalData.imgSrc = obj.url;
+			wx.navigateBack({
+				delta: -1
+			})
+		});
+	},
+	rotate() {
+		//在用户旋转的基础上旋转90°
+		this.cropper.setAngle(this.cropper.data.angle += 90);
+	},
+	top() {
+		this.data.top = setInterval(() => {
+			this.cropper.setTransform({
+				y: -3
+			});
+		}, 1000 / 60)
+	},
+	bottom() {
+		this.data.bottom = setInterval(() => {
+			this.cropper.setTransform({
+				y: 3
+			});
+		}, 1000 / 60)
+	},
+	left() {
+		this.data.left = setInterval(() => {
+			this.cropper.setTransform({
+				x: -3
+			});
+		}, 1000 / 60)
+	},
+	right() {
+		this.data.right = setInterval(() => {
+			this.cropper.setTransform({
+				x: 3
+			});
+		}, 1000 / 60)
+	},
+	narrow() {
+		this.data.narrow = setInterval(() => {
+			this.cropper.setTransform({
+				scale: -0.02
+			});
+		}, 1000 / 60)
+	},
+	enlarge() {
+		this.data.enlarge = setInterval(() => {
+			this.cropper.setTransform({
+				scale: 0.02
+			});
+		}, 1000 / 60)
+	},
+	end(e) {
+		clearInterval(this.data[e.currentTarget.dataset.type]);
+	},
 })
